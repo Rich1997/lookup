28cc226d6931487447cd4fc470f544f954c52731
Rich1997 <rudronil1997@gmail.com>
2022-05-23 23:04:16 +0530

    Update Searchbar.jsx styling

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -23,14 +23,14 @@ const Searchbar = () => {
     return (
         <div>
             <div className="flex items-center justify-center">
-                <div className="p-8 flex items-center gap-4 w-full md:w-[32rem] h-24 dark:bg-zinc-800/70 bg-[#fff] dark:shadow-2xl dark:shadow-black shadow-xl shadow-stone-300/50 dark:text-zinc-500 text-zinc-300">
+                <div className="flex items-center gap-4 w-full md:w-[32rem] h-24 dark:bg-zinc-800/70 bg-[#fff] dark:shadow-2xl dark:shadow-black shadow-xl shadow-stone-300/50 dark:text-zinc-500 text-zinc-300">
                     <input
-                        className="w-full py-4 bg-transparent dark:text-zinc-50 text-zinc-800 dark:caret-zinc-400 caret-zinc-800 focus:outline-none tracking-tight placeholder:text-zinc-400"
+                        className="w-full h-full py-4 pl-8 bg-transparent dark:text-zinc-50 text-zinc-800 dark:caret-zinc-400 caret-zinc-800 focus:outline-none tracking-tight placeholder:text-zinc-400"
                         placeholder=" look up your word..."
                         onChange={handleInputChange}
                         onKeyDown={handleInputKeyDown}
                     ></input>
-                    <div className="hover:opacity-50" onClick={handleSubmit}>
+                    <div className="pr-8 hover:opacity-50" onClick={handleSubmit}>
                         <Search />
                     </div>
                 </div>
